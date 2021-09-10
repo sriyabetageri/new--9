7f65b342fcad5dab6fc7ef16caecb7daed0051c3
sriyabetageri <75461862+sriyabetageri@users.noreply.github.com>
2021-09-09 19:27:37 -0700

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import StackNavigator from "./StackNavigator";
+import Profile from "../screens/Profile";
+import Logout from "../screens/Logout";
+import firebase from "firebase";
+import { Component } from "react/cjs/react.production.min";
+import CustomSideBarMenu from "../screens/CustomSideBarMenu";
+
+const Drawer = createDrawerNavigator();
+
+export default class DrawerNavigator extends Component{
+constructor(props){
+  super(props);
+  this.state={
+    light_theme:true
+  }
+}
+componentDidMount(){
+  let theme;
+  firebase
+  .database()
+  .ref("/users/"+firebase.auth().currentUser.uid)
+  .on("value",function(snapshot){
+    theme = snapshot.val().current_theme
+  })
+  this.setState({light_theme: theme==="light"? true:false})
+}
+render (){
+  return(
+    <DrawerNavigator 
+    drawerContentOptions={{
+      activeTintColor:"#e91e63",
+      inactiveColor:this.state.light_theme? "black":"white", 
+      itemStyle: {marginVertical:5},
+
+    }}
+    drawerContent = {(props)=> <CustomSideBarMenu{...props}/>}
+    >
+      <Drawer.Screen name = "Home" component = {StackNavigator} options = {{unmountOnBlur:true}}/>
+      <Drawer.Screen name = "Profile" component = {Profile} options = {{unmountOnBlur:true}}/>
+      <Drawer.Screen name = "Logout" component = {Logout} options = {{unmountOnBlur:true}}/>
+
+    </DrawerNavigator>
+  )
+}
+}
+  
